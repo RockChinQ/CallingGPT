ae061a7365d51156dc060e44e798f582cc0448cd
Junyan Qin <1010553892@qq.com>
2023-06-19 03:19:29 +0000

    refactor(namespace.py): decouple func schema gen logic

--- CallingGPT/entities/namespace.py
+++ CallingGPT/entities/namespace.py
@@ -46,7 +46,79 @@ class Namespace:
     def _func_schema(self, function: callable) -> dict:
         """
         Return the data schema of a function.
+        {
+            "function": function,
+            "description": "function description",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "parameter_a": {
+                        "type": "str",
+                        "description": "parameter_a description"
+                    },
+                    "parameter_b": {
+                        "type": "int",
+                        "description": "parameter_b description"
+                    },
+                    "parameter_c": {
+                        "type": "str",
+                        "description": "parameter_c description",
+                        "enum": ["a", "b", "c"]
+                    },
+                },
+                "required": ["parameter_a", "parameter_b"]
+            }
+        }
         """
+        func_doc = function.__doc__
+        # Google Style Docstring
+        if func_doc is None:
+            raise Exception("Function {} has no docstring.".format(function.__name__))
+        func_doc = func_doc.strip().replace('    ','').replace('\t', '')
+        # extract doc of args from docstring
+        doc_spt = func_doc.split('\n\n')
+        desc = doc_spt[0]
+        args = doc_spt[1] if len(doc_spt) > 1 else ""
+        returns = doc_spt[2] if len(doc_spt) > 2 else ""
+
+        # extract args
+        # delete the first line of args
+        arg_lines = args.split('\n')[1:]
+        arg_doc_list = re.findall(r'(\w+)(\((\w+)\))?:\s*(.*)', args)
+        args_doc = {}
+        for arg_line in arg_lines:
+            doc_tuple = re.findall(r'(\w+)(\((\w+)\))?:\s*(.*)', arg_line)
+            if len(doc_tuple) == 0:
+                continue
+            args_doc[doc_tuple[0][0]] = doc_tuple[0][3]
+
+        # extract returns
+        return_doc_list = re.findall(r'(\w+):\s*(.*)', returns)
+
+        params = enumerate(inspect.signature(function).parameters.values())
+        parameters = {
+            "type": "object",
+            "required": [],
+            "properties": {},
+        }
+
+
+        for i, param in params:
+            param_type = param.annotation.__name__
+            if param_type == "str":
+                param_type = "string"
+            parameters['properties'][param.name] = {
+                "type": param_type,
+                "description": args_doc[param.name],
+            }
+            if param.default is inspect.Parameter.empty:
+                parameters["required"].append(param.name)
+
+        return {
+            "function": function,
+            "description": desc,
+            "parameters": parameters,
+        }
 
     def _retrieve_functions(self):
         self.functions = {}
@@ -61,58 +133,10 @@ class Namespace:
             self.functions[module.__name__.replace(".","-")] = {}
 
             for name, function in functions.items():
-                
-                func_doc = function.__doc__
-                # Google Style Docstring
-                if func_doc is None:
-                    raise Exception("Function {} has no docstring.".format(name))
-                func_doc = func_doc.strip().replace('    ','').replace('\t', '')
-                # extract doc of args from docstring
-                doc_spt = func_doc.split('\n\n')
-                desc = doc_spt[0]
-                args = doc_spt[1] if len(doc_spt) > 1 else ""
-                returns = doc_spt[2] if len(doc_spt) > 2 else ""
-
-                # extract args
-                # delete the first line of args
-                arg_lines = args.split('\n')[1:]
-                arg_doc_list = re.findall(r'(\w+)(\((\w+)\))?:\s*(.*)', args)
-                args_doc = {}
-                for arg_line in arg_lines:
-                    doc_tuple = re.findall(r'(\w+)(\((\w+)\))?:\s*(.*)', arg_line)
-                    if len(doc_tuple) == 0:
-                        continue
-                    args_doc[doc_tuple[0][0]] = doc_tuple[0][3]
-
-                # extract returns
-                return_doc_list = re.findall(r'(\w+):\s*(.*)', returns)
-
-                params = enumerate(inspect.signature(function).parameters.values())
-                parameters = {
-                    "type": "object",
-                    "required": [],
-                    "properties": {},
-                }
-
-
-                for i, param in params:
-                    param_type = param.annotation.__name__
-                    if param_type == "str":
-                        param_type = "string"
-                    parameters['properties'][param.name] = {
-                        "type": param_type,
-                        "description": args_doc[param.name],
-                    }
-                    if param.default is inspect.Parameter.empty:
-                        parameters["required"].append(param.name)
-
-                funtion_dict = {
-                    "function": function,
-                    "description": desc,
-                    "parameters": parameters,
-                }
+                funtion_dict = self._func_schema(function)
 
                 self.functions[module.__name__.replace(".","-")][name] = funtion_dict
+        print(self.functions)
 
     def __init__(self, modules: list):
         self.modules = modules
