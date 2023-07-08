69321c0740de76408c3c395e4c01f1a6f0b35d97
Junyan Qin <1010553892@qq.com>
2023-07-08 01:50:20 +0000

    style: simplify code

--- src/CallingGPT/session/session.py
+++ src/CallingGPT/session/session.py
@@ -62,19 +62,7 @@ class Session:
                 })
 
                 self.messages = messages.copy()
-
-                ret = {
-                    "type": "function_call",
-                    "func": fc['name'].replace('-', '.'),
-                    "value": call_ret,
-                }
-
             else:
-                ret = {
-                    "type": "message",
-                    "value": reply_msg['content'],
-                }
-
                 messages.append({
                     "role": "assistant",
                     "content": reply_msg['content']
@@ -83,62 +71,6 @@ class Session:
                 self.messages = messages.copy()
 
                 break
-
-    # def ask(self, msg: str) -> dict:
-    #     self.messages.append(
-    #         {
-    #             "role": "user",
-    #             "content": msg
-    #         }
-    #     )
-
-    #     args = {
-    #         "model": self.model,
-    #         "messages": self.messages,
-    #     }
-
-    #     if len(self.namespace.functions_list) > 0:
-    #         args['functions'] = self.namespace.functions_list
-    #         args['function_call'] = "auto"
-
-    #     resp = openai.ChatCompletion.create(
-    #         **args
-    #     )
-
-    #     logging.debug("Response: {}".format(resp))
-    #     reply_msg = resp["choices"][0]['message']
-
-    #     ret = {}
-
-    #     if 'function_call' in reply_msg:
-
-    #         fc = reply_msg['function_call']
-    #         args = json.loads(fc['arguments'])
-    #         call_ret = self._call_function(fc['name'], args)
-
-    #         self.messages.append({
-    #             "role": "function",
-    #             "name": fc['name'],
-    #             "content": str(call_ret)
-    #         })
-
-    #         ret = {
-    #             "type": "function_call",
-    #             "func": fc['name'].replace('-', '.'),
-    #             "value": call_ret,
-    #         }
-    #     else:
-    #         ret = {
-    #             "type": "message",
-    #             "value": reply_msg['content'],
-    #         }
-
-    #         self.messages.append({
-    #             "role": "assistant",
-    #             "content": reply_msg['content']
-    #         })
-
-    #     return ret
 
     def _call_function(self, function_name: str, args: dict):
         return self.namespace.call_function(function_name, args)
